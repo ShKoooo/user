6fca4d307fcd837ec0b47ec30008bdab5b7fa9f5
Sanghoon Koo <Sanghoon Koo@DESKTOP-KK16A9F>
2021-11-03 11:59:30 +0900

    MemberDTO_SH

--- campingutte/src/main/java/com/member/MemberDTO.java
+++ campingutte/src/main/java/com/member/MemberDTO.java
@@ -1,5 +1,82 @@
 package com.member;
 
 public class MemberDTO {
-
+	private String memberId;
+	private String memberName;
+	private String memberPwd;
+	private String memberRegdate;
+	private String memberUpdate;
+	private String memberBirth;
+	private String memberEmail;
+	private String memberTel;
+	private String memberAddr;
+	private String memberAddr2;
+	private int memberPoint;
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+	public String getMemberRegdate() {
+		return memberRegdate;
+	}
+	public void setMemberRegdate(String memberRegdate) {
+		this.memberRegdate = memberRegdate;
+	}
+	public String getMemberUpdate() {
+		return memberUpdate;
+	}
+	public void setMemberUpdate(String memberUpdate) {
+		this.memberUpdate = memberUpdate;
+	}
+	public String getMemberBirth() {
+		return memberBirth;
+	}
+	public void setMemberBirth(String memberBirth) {
+		this.memberBirth = memberBirth;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberTel() {
+		return memberTel;
+	}
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+	public String getMemberAddr() {
+		return memberAddr;
+	}
+	public void setMemberAddr(String memberAddr) {
+		this.memberAddr = memberAddr;
+	}
+	public String getMemberAddr2() {
+		return memberAddr2;
+	}
+	public void setMemberAddr2(String memberAddr2) {
+		this.memberAddr2 = memberAddr2;
+	}
+	public int getMemberPoint() {
+		return memberPoint;
+	}
+	public void setMemberPoint(int memberPoint) {
+		this.memberPoint = memberPoint;
+	}
 }
