c54c938a0eba360c9028cc11ca8c12fd1d7eefb3
Sanghoon Koo <Sanghoon Koo@DESKTOP-KK16A9F>
2021-11-12 16:59:34 +0900

    review시작_shk

---
+++ campingutte/src/main/java/com/review/ReviewDTO.java
@@ -0,0 +1,47 @@
+package com.review;
+
+public class ReviewDTO {
+	private String reviewNo;
+	private String reviewComment;
+	private String reviewDate;
+	private String memberId;
+	private String bookNo;
+	private String reviewStar;
+	
+	public String getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(String reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public String getReviewComment() {
+		return reviewComment;
+	}
+	public void setReviewComment(String reviewComment) {
+		this.reviewComment = reviewComment;
+	}
+	public String getReviewDate() {
+		return reviewDate;
+	}
+	public void setReviewDate(String reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(String bookNo) {
+		this.bookNo = bookNo;
+	}
+	public String getReviewStar() {
+		return reviewStar;
+	}
+	public void setReviewStar(String reviewStar) {
+		this.reviewStar = reviewStar;
+	}
+}
